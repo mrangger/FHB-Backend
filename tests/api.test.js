715516d156d8aad493443d608545fd454472d124
Mathias Rangger <mathias.rangger@gmail.com>
2023-09-21 15:04:55 +0000

    Add eslint

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,25 +1,27 @@
-const supertest = require('supertest')
-const app = require('../index.js')
+const supertest = require('supertest');
+const app = require('../index.js');
 
-const api = supertest(app)
+const api = supertest(app);
 
 test('notes are returned as json', async () => {
   await api
-    .get('/api/notes')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+      .get('/api/notes')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+});
 
 
 test('there are two notes', async () => {
-    const response = await api.get('/api/notes')
-    expect(response.body).toHaveLength(3)
-  })
-  
+  const response = await api.get('/api/notes');
+  expect(response.body).toHaveLength(3);
+});
+
 test('the first note is about HTTP methods', async () => {
-  const response = await api.get('/api/notes')  
-  expect(response.body[0].content).toBe('HTML is easy')
-})
+  const response = await api.get('/api/notes');
+  expect(response.body[0].content).toBe('HTML is easy');
+});
 
 
-afterAll(() => { console.log('closing...'); app.close(); });
+afterAll(() => {
+  console.log('closing...'); app.close();
+});
